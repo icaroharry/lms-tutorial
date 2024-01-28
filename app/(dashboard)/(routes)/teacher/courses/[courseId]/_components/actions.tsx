abefeef79c3e03a48b1d743f315d807f55846cdd
Ícaro Harry <icaro.harry@toptal.com>
2024-01-28 15:49:43 -0300

    Remove .env file

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -47,6 +47,7 @@ export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
       setIsLoading(true);
       await axios.delete(`/api/courses/${courseId}`);
       toast.success("Course deleted");
+      router.refresh();
       router.push(`/teacher/courses`);
     } catch (error) {
       toast.error("Something went wrong");
